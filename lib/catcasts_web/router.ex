337ccca9b4e549060faa37dc5bbee67a1b52ff6b
MeLeRyZ <meleryz@gmail.com>
2018-03-03 23:16:25 +0200

    add google authentication using ueberauth and google api keys

--- lib/catcasts_web/router.ex
+++ lib/catcasts_web/router.ex
@@ -19,6 +19,13 @@ defmodule CatcastsWeb.Router do
     get "/", PageController, :index
   end
 
+  scope "/auth", CatcastsWeb do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :request #for any auth. google, facebook and others
+    get "/:provider/callback", AuthController, :new
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", CatcastsWeb do
   #   pipe_through :api
