eca052369d35ff296f6a20134773293cff66f1c8
MeLeRyZ <meleryz@gmail.com>
2018-03-03 23:25:47 +0200

    finished 4th tutorial for google authentication

--- lib/catcasts_web/controllers/auth_controller.ex
+++ lib/catcasts_web/controllers/auth_controller.ex
@@ -13,4 +13,27 @@ defmodule CatcastsWeb.AuthController do
     changeset = User.changeset(%User{}, user_params)
     create(conn, changeset)
   end
+
+  def create(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Thank you for signing in!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: page_path(conn, :index))
+      {:index, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: page_path(conn, :index))
+    end
+  end
+
+  def insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+  end
 end
