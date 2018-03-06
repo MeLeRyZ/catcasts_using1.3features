a826c613f3126bdbe114af127075b5082788313e
MeLeRyZ <meleryz@gmail.com>
2018-03-06 15:16:50 +0200

    make video section and redirection. next step is to make json understandable with youtube api

--- lib/catcasts_web/controllers/auth_controller.ex
+++ lib/catcasts_web/controllers/auth_controller.ex
@@ -24,7 +24,7 @@ defmodule CatcastsWeb.AuthController do
         conn
         |> put_flash(:info, "Thank you for signing in!")
         |> put_session(:user_id, user.id)
-        |> redirect(to: page_path(conn, :index))
+        |> redirect(to: video_path(conn, :index))
 
       {:index, _reason} ->
         conn
--- test/catcasts_web/templates/layout/navigation_test.exs
+++ test/catcasts_web/templates/layout/navigation_test.exs
@@ -1,9 +1,9 @@
 defmodule CatcastsWeb.NavigationTest do
-  use CatcastsWeb.ConnCase, async: true
+  use CatcastsWeb.ConnCase
   import Catcasts.Factory
 
-  test "Shows a signin with google link when not signed in", %{conn: conn} do
-    conn = get conn, "/"
+  test "shows a sign in with Google link when not signed in", %{conn: conn} do
+    conn = get(conn, "/")
 
     assert html_response(conn, 200) =~ "Sign in with Google"
   end
@@ -11,11 +11,28 @@ defmodule CatcastsWeb.NavigationTest do
   test "shows a sign out link when signed in", %{conn: conn} do
     user = insert(:user)
 
-    conn = conn
-    |> assign(:user, user)
-    |> get("/")
+    conn =
+      conn
+      |> assign(:user, user)
+      |> get("/")
 
     assert html_response(conn, 200) =~ "Sign out"
   end
 
+  test "shows a link to the videos index", %{conn: conn} do
+    conn = get(conn, "/")
+
+    assert html_response(conn, 200) =~ "<a href=\"/videos\">Videos</a>"
+  end
+
+  test "shows a link to add video for a signed in user", %{conn: conn} do
+    user = insert(:user)
+
+    conn =
+      conn
+      |> assign(:user, user)
+      |> get("/")
+
+    assert html_response(conn, 200) =~ "<a href=\"/videos/new\">Add video</a>"
+  end
 end
